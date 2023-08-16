3b7258a57e9795f1477dfa9327496cc2c91a558f
chi1034 <odiigift12@gmail.com>
2023-08-16 14:32:56 -0700

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,48 +1,18 @@
 #include <stdio.h>
-#include "main.h"
 /**
- * print_times_table - prints the n times table, starting with 0
- * @n: number of the times table
+ * main - Entry point
+ * Description: Lists all the natural numbers below 1024
+ * Return: always 0
  */
-void print_times_table(int n)
-
+int main(void)
 {
-	int i, j, k;
-	if (n >= 0 && n <= 15)
-	{
-		for (i = 0; i <= n; i++)
-		{
-			for (j = 0; j <= n; j++)
-			{
-				k = j * i;
-				if (j == 0)
-				{
-					_putchar(k + '0');
-				} else if (k < 10 && j != 0)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(k + '0');
-				} else if (k >= 10 && k < 100)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar((k / 10) + '0');
-					_putchar((k % 10) + '0');
-				} else if (k >= 100)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar((k / 100) + '0');
-					_putchar(((k / 10) % 10) + '0');
-					_putchar((k % 10) + '0');
-				}
-			}
-			_putchar('\n');
-		}
-	}
-
+        int a;
+        int sum = 0;
+        for (a = 0; a < 1024; a++)
+        {
+                if ((a % 3) == 0 || (a % 5) == 0)
+                        sum += a;
+        }
+        printf("%d\n", sum);
+        return (0);
 }
